25aeb123ce92a07507c0744af87efc839670e861
Patrick Nagel <patrick.nagel@smail.inf.h-brs.de>
2019-04-22 18:42:24 +0200

    Add the instruction and required data.
    This commit adds the instructions for assignment 3 and the required data as well as files that serve as templates for the code.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Assignment 3
+In this Assignment you will be implementing Breadth First Search, Depth First Search and Iterative Deepening Depth First Search.
+Please read the PDF file in this repository to get an idea of the problem statement.
+
+## Coding Guidelines
+* Please use .py files provided to code your solutions.
+* A minimalistic template is provided so that you can organize your ideas and put them into the appropriate functions.
+* Please fill out the docstrings (function args and return value type and description) after implementation.
+* Please add any extra functions if needed in the helper.py.
+* Do not clutter your code and try to be more Pythonic in coding.
+* Kindly use meaningful identifiers for variable/function/class names. Also add comments to help us easily evaluate your code.
+
+#### A Failure to adhere to the above instructions Will result in a loss of points.
---
+++ src/helper.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  3 01:36:01 2018
+
+@author: Iswariya Manivannan
+"""
+
+import sys
+import os
+import time
+
+
+def maze_map_to_tree(maze_map):
+    """Function to create a tree from the map file. The idea is
+    to check for the possible movements from each position on the
+    map and encode it in a data structure like list.
+
+    Parameters
+    ----------
+    maze_map : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+
+
+    raise NotImplementedError
+
+
+def assign_character_for_nodes(maze_map, current_node, prev_node):
+    """Function to assign character for the visited nodes. Please assign
+    meaningful characters based on the direction of tree traversal.
+
+    Parameters
+    ----------
+    maze_map : [type]
+        [description]
+    current_node : [type]
+        [description]
+    prev_node : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+
+
+    raise NotImplementedError
+
+
+def write_output(path):
+    """Function to write output to console and a txt file.
+    Please ensure that it should be possible to visualize each and every
+    step of the tree traversal algorithm in the map in the console.
+    This enables understanding towards the working of your
+    tree traversal algorithm as to how it reaches the goals.
+
+    Parameters
+    ----------
+    path : [type]
+        [description]
+
+    """
+
+    raise NotImplementedError
